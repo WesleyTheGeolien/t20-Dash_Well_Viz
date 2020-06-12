f3d3f2a4ea46e735117e4cc643342e96bd4d6f21
Wesley Banfield <wesleybanfield@gmail.com>
2020-06-12 21:38:45 +0200

    remove uncessary imports

--- simple_dash_layout/dash_app.py
+++ simple_dash_layout/dash_app.py
@@ -4,10 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-
-# import plotly.express as px
 import plotly.graph_objs as go
-# from plotly.subplots import make_subplots
 
 import dashwellviz.figures
 import helper
