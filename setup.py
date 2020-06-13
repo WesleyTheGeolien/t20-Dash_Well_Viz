42b2cd791f212f1ed8a6e6112f85854b410f9b8a
Wesley Banfield <wesleybanfield@gmail.com>
2020-06-13 23:05:11 +0200

    test mod setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup
 
 setup(
     name="dashwellviz",
-    use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Tools for Transform 2020 Dash_Well_Viz project",
     long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
     url="https://github.com/WesleyTheGeolien/t20-Dash_Well_Viz",
     author="Transform 2020 Hackathon Dash_Well_Viz contributors",
     packages=["dashwellviz"],
