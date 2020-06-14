4d3147ec3fa449b8bfcf1910d3fb196519d68645
Michael Harty <michaelharty3@gmail.com>
2020-06-13 23:27:45 -0500

    select wells from checkbox

--- simple_dash_layout/dash_app.py
+++ simple_dash_layout/dash_app.py
@@ -21,12 +21,11 @@ well_names_labels_dict = [{'label': w, 'value': w} for w in list_of_well_names]
 
 
 # Create log plot from prebuilt figures
-log = dashwellviz.figures.make_composite_log(
-    data_df, lines=[["Vp"], ["Vs"], ["HROM"], ["ATRX", "ATRT"]], log_tracks=[3]
-)
+# In order to work with the checkbox idea, plot constructor will need to accept a list of logs.
+# I think that means for now, that means we are restricted to only one log per track, and no logoritmic tracks. 
+# We will need to find a better way to select the logs and define track properties
 
-log_trace_fig = log.fig
-log_trace_fig.update_layout(template='plotly_white', height=800, width=800)
+log_trace_fig = helper.composite_plot_from_list_of_log_names(data_df, ['ECGR', 'Vp', 'Vs', 'HROM'])
 
 # TODO this needs to be abstracted
 # make cross plot 
@@ -63,7 +62,10 @@ app.layout = html.Div([
         html.Div(className='sidebar', children=[
             html.H1('Sidebar'),
             dcc.Dropdown(id='well-selector', placeholder="Select a well", options=well_names_labels_dict, value='Poseidon 1'),
-            dcc.Checklist(id='curve-selectors', options=data_labels_dict, style={'display': 'inline-block'}), # TODO: layout checkbox elements
+            dcc.Checklist(id='curve-selectors', 
+                          options=data_labels_dict, 
+                          style={'display': 'inline-block'}, 
+                          value=['ECGR', 'Vp', 'Vs', 'HROM']), # TODO: properly layout checkbox elements
             
             html.H2('Crossplot Options'),
 
@@ -88,7 +90,7 @@ app.layout = html.Div([
         html.Div([
             html.H1('Poseidon 1', id='log-plot-header', style={'text-align': 'center'}),
             html.Div(className='well-plot-container', children=[
-                dcc.Graph(figure=log_trace_fig) 
+                dcc.Graph(id='log-trace-plot', figure=log_trace_fig) 
             ]),
         ]),
     
@@ -144,6 +146,12 @@ def update_cross_plot(y_axis, x_axis, color):
 def update_well_name_in_title(value):
     return value
 
+# Choose the displayed log curves from checkbox
+@app.callback(
+    Output('log-trace-plot', 'figure'),
+    [Input('curve-selectors', 'value')])
+def update_log_plots_on_curve_selection(curve_names):
+    return helper.composite_plot_from_list_of_log_names(data_df, curve_names)
 
 # Run the app
 if __name__ == '__main__':
